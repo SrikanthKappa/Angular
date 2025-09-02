c82d2721c2dca4dea19cd9a53d1465392e3a30a8
srikanth <srikanthkappa16@gmail.com>
2025-09-02 08:34:45 +0530

    initial Angular commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularComponents';
+  city:string="Anantapuram City";
+  d:string=new Date().toLocaleTimeString();
+  settimeout=setInterval(()=>{
+    this.d=new Date().toLocaleTimeString();
+  },1000);
+
 }
